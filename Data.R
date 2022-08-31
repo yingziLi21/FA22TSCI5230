78dff33f719bda351c33ce94da989bb0a2833142
Yingzi Li <yingz@yingzis-air.med.uthscsa.edu>
2022-08-31 16:40:17 -0500

    file for downloading data

---
+++ Data.R
@@ -0,0 +1,70 @@
+#'---
+#' title: "Data Extraction
+#' author: 'Yingzi'
+#' abstract: |
+#'  | Provide a summary of objectives, study design, setting, participants,
+#'  | sample size, predictors, outcome, statistical analysis, results,
+#'  | and conclusions.
+#' documentclass: article
+#' description: 'Manuscript'
+#' clean: false
+#' self_contained: true
+#' number_sections: false
+#' keep_md: true
+#' fig_caption: true
+#' output:
+#'  html_document:
+#'    toc: true
+#'    toc_float: true
+#'    code_folding: show
+#' ---
+#'
+#+ init, echo=FALSE, message=FALSE, warning=FALSE
+# init ----
+# This part does not show up in your rendered report, only in the script,
+# because we are using regular comments instead of #' comments
+debug <- 0;
+knitr::opts_chunk$set(echo=debug>-1, warning=debug>0, message=debug>0);
+
+
+
+library(rio);# simple command for importing and exporting
+library(pander); # format tables
+library(printr); # set limit on number of lines printed
+library(broom); # allows to give clean dataset
+library(dplyr); #add dplyr library
+library(fs)
+
+options(max.print=42);
+panderOptions('table.split.table',Inf); panderOptions('table.split.cells',Inf);
+whatisthis <- function(xx){
+  list(class=class(xx),info=c(mode=mode(xx),storage.mode=storage.mode(xx)
+                              ,typeof=typeof(xx)))};
+
+#' # Import the data
+InputData <- 'https://physionet.org/static/published-projects/mimic-iv-demo/mimic-iv-clinical-database-demo-1.0.zip'
+dir.create("data", showWarnings = FALSE)
+ZippedData <- file.path("data", "temptdata.zip")
+download.file(InputData,destfile = ZippedData)
+
+# UnzippedDate the data
+UnzippedData <- unzip(ZippedData,exdir= "data" ) %>%
+grep ("gz",., value = TRUE)
+
+TableNames <- path_ext_remove(UnzippedData)  %>% path_ext_remove() %>% path_ext_remove()
+# for (ii in seq_along(TableNames)){
+#   assign(TableNames[ii], import(UnzippedData[ii]), format ='CSV'));
+
+mapply(function(xx,yy)
+  assign(xx,import(yy,format = 'CSV'),inherits = TRUE),TableNames,UnzippedData)
+
+save(list = TableNames,file='data.R.rdata');
+if(!file.exists(ZippedData)) {download.file(InputData, destfile = ZippedData)}
+
+# %>%
+
+# grep("gz", UnzippedData) # poositions in the vector where the pottern "gz" has been fpund
+# grep("gz", UnizippedData, value = TURE) #return the actual strings
+
+
+
