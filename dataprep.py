1824d2a8e1a745ef6a24baa82c06218aedac62db
Yingzi Li <yingz@yingzis-air.med.uthscsa.edu>
2022-11-30 14:37:05 -0600

    29

--- dataprep.py
+++ dataprep.py
@@ -15,5 +15,19 @@ dd.keys()
 ## dd['admissions'] #get individual table
 
 #Create table: demographics from admissions, patients
-demographics=dd["admission"].copy() # create demographics table copy from admissions
-  
+demographics=dd["admissions"].copy() # create demographics table copy from admissions
+patients=dd["patients"].copy().drop('dod', axis=1) #
+
+
+#Create LOS variable
+demographics['LOS']=(pd.to_datetime(demographics['dischtime']) - 
+pd.to_datetime(demographics['admittime']))/np.timedelta64(1, 'D')
+
+
+demographics1= demographics.groupby('subject_id').agg(admits=('subject_id', 'count'),
+  eth=('ethnicity', 'nunique'),
+  ethnicity_como=('ethnicity', lambda xx:':'.join(sorted(list(set(xx))))),
+  language=('language','last'),
+  dod=('deathtime',lambda xx: max(pd.to_datetime(xx))),
+  los=('LOS',np.median),
+  numED=('edregtime', lambda xx: xx.notnull().sum())).reset_index(drop=False).merge(patients, on='subject_id')
